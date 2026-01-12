cc9f0fa7aaddbd47c83b438deb0c407d9f0da813
Pedro <130769202+phbrugnolo@users.noreply.github.com>
2026-01-12 17:11:46 -0300

    Improve Docker installation script in workspace.sh
    Refactor Docker installation process and update commands.

--- workspace.sh
+++ workspace.sh
@@ -6,21 +6,24 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install curl gpg gcc g++ make zip plocate jq -y
 
 # docker installation
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get update
-sudo apt-get install -y ca-certificates
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+sudo apt update
+sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker $USER
 
 ## Restart VM
-
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
 
 # This folder is necessary to connect with pgAdmin automatically
 mkdir -p ~/pgadmin_servers
